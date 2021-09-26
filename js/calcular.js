f3b6492607aa77f89a90cc6d41f1bc54038ce11b
KEVINMYG <matsudacanotojhoel2@hotmail.com>
2021-09-25 20:44:51 -0500

    Clase Nº02

---
+++ js/calcular.js
@@ -0,0 +1,7 @@
+function calcular(){
+    var n1=Number(document.getElementById('n1').value);
+    var n2=Number(document.getElementById('n2').value);
+           var t33=(n1+n2)/2;
+           document.getElementById('prom').value=t33
+
+}
